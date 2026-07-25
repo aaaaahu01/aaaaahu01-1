ce2e5dda05f1be67e0e9a1e0c4144e1845dbb374
aaaaahu <huyunxi40@gmail.com>
2026-07-25 14:39:01 +0800

    推送vue基本项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <h1>nihao</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name:'App'
+}
+</script>
+
+<style>
+.app {
+    background-color:aliceblue;
+}
+</style>
